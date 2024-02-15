78b58f73913669fd8e31bd71cac9af95d583e094
ravine13 <kuriaderek@gmail.com>
2024-02-15 04:30:37 +0300

    set up (#1)
    * set up
    
    * new data

--- app/models.py
+++ app/models.py
@@ -5,39 +5,42 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
 class User(db.Model):
     __tablename__ = 'user'
     id = Column(UUID, primary_key=True)
-    email = Column(String)
     password = Column(String)
-    role = Column(Integer)
     confirmed = Column(Boolean)
+    role = Column(Integer)
     created_at = Column(DateTime)
+    email = Column(String)
 
 class Profile(db.Model):
     __tablename__ = 'Profile'
     id = Column(UUID, primary_key=True)
+    user = relationship('User', backref='profiles')
     user_id = Column(UUID, ForeignKey('user.id'))
     first_name = Column(String)
     last_name = Column(String)
     profile_photo = Column(String)
-    user = relationship('User', backref='profiles')
+    
 
 class Interests(db.Model):
     __tablename__ = 'Interests'
     id = Column(UUID, primary_key=True)
-    event_id = Column(UUID)
+    event_id = Column(UUID, ForeignKey('Event.id'))
     user_id = Column(UUID, ForeignKey('user.id'))
     user = relationship('User', backref='interests')
+    event = relationship('Event', backref='interests')
 
 class Tag(db.Model):
     __tablename__ = 'tag'
     id = Column(UUID, primary_key=True)
     user_id = Column(UUID, ForeignKey('user.id'))
     name = Column(String)
-    event_id = Column(UUID)
+    event_id = Column(UUID, ForeignKey('Event.id'))
+    event = relationship('Event', backref='tags')
     user = relationship('User', backref='tags')
+    
 
 class Event(db.Model):
     __tablename__ = 'Event'
@@ -45,78 +48,77 @@ class Event(db.Model):
     name = Column(String)
     description = Column(String)
     organiser_id = Column(UUID, ForeignKey('user.id'))
+    organiser = relationship('User', backref='events')
     start_date = Column(DateTime)
     start_time = Column(DateTime)
     end_date = Column(DateTime)
     end_time = Column(DateTime)
     duration = Column(String)
     venue = Column(String)
-    photo_id = Column(UUID)
+    photo_id = Column(UUID, ForeignKey('Photo.id'))
     created_at = Column(DateTime)
-    organiser = relationship('User', backref='events')
+    photo = relationship('Photo', backref='events')
 
 class Billing_Info(db.Model):
     __tablename__ = 'Billing_Info'
     id = Column(UUID, primary_key=True)
-    user_id = Column(UUID, ForeignKey('user.id'))
     payment_method = Column(Enum('credit_card', 'm_pesa', 'airtel_money', name='Ptype'))
-    payment_details_id = Column(UUID)
+    payment_details_id = Column(UUID, ForeignKey('Billing_Details.id'))
+    billing_details = relationship('Billing_Details', backref='billing_infos')
+    user_id = Column(UUID, ForeignKey('user.id'))
     user = relationship('User', backref='billing_infos')
 
-class Payment_Details(db.Model):
-    __tablename__ = 'Payment_Details'
+class Billing_Details(db.Model):
+    __tablename__ = 'Billing_Details'
     id = Column(UUID, primary_key=True)
-    name = Column(String)
     detail = Column(String)
+    name = Column(String)
+    
 
-class Event_Billing(db.Model):
-    __tablename__ = 'Event_Billing'
+class Advert_Fees(db.Model):
+    __tablename__ = 'Advert_Fees'
     id = Column(UUID, primary_key=True)
+    user_id = Column(UUID, ForeignKey('user.id'))
+    user = relationship('User', backref='advert_fees')
+    amount = Column(Float)
+    event = relationship('Event', backref='advert_fees')
     event_id = Column(UUID, ForeignKey('Event.id'))
-    billing_id = Column(UUID, ForeignKey('Billing_Info.id'))
-    event = relationship('Event', backref='event_billings')
-    billing_info = relationship('Billing_Info', backref='event_billings')
+    created_at = Column(DateTime)
+    
+    
 
 class Pricing(db.Model):
     __tablename__ = 'Pricing'
     event_id = Column(UUID, ForeignKey('Event.id'))
+    event = relationship('Event', backref='pricings')
     id = Column(UUID, primary_key=True)
     name = Column(String)
     amount = Column(Float)
-    event = relationship('Event', backref='pricings')
+    
 
 class Review(db.Model):
     __tablename__ = 'Review'
     id = Column(UUID, primary_key=True)
     user_id = Column(UUID, ForeignKey('user.id'))
     event_id = Column(UUID, ForeignKey('Event.id'))
-    comment = Column(String)
-    rating = Column(Integer)
-    user = relationship('User', backref='reviews')
     event = relationship('Event', backref='reviews')
-
-class Payment(db.Model):
-    __tablename__ = 'Payment'
-    id = Column(UUID, primary_key=True)
-    user_id = Column(UUID, ForeignKey('user.id'))
-    event_id = Column(UUID, ForeignKey('Event.id'))
-    pricing_id = Column(UUID, ForeignKey('Pricing.id'))
-    date_created = Column(DateTime)
-    payment_method = Column(String)
-    user = relationship('User', backref='payments')
-    event = relationship('Event', backref='payments')
-    pricing = relationship('Pricing', backref='payments')
+    rating = Column(Integer)
+    comment = Column(String)
+    user = relationship('User', backref='reviews')
+    
+    
 
 class Booking(db.Model):
     __tablename__ = 'Booking'
     id = Column(UUID, primary_key=True)
     event_id = Column(UUID, ForeignKey('Event.id'))
     user_id = Column(UUID, ForeignKey('user.id'))
-    pricing_id = Column(UUID, ForeignKey('Pricing.id'))
-    date_created = Column(DateTime)
     user = relationship('User', backref='bookings')
-    event = relationship('Event', backref='bookings')
+    pricing_id = Column(UUID, ForeignKey('Pricing.id'))
     pricing = relationship('Pricing', backref='bookings')
+    date_created = Column(DateTime)
+    event = relationship('Event', backref='bookings')
+    
 
 class Photo(db.Model):
     __tablename__ = 'Photo'
