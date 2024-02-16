f450a4318894f150d3366f77c70a1c43a7e1bf04
Adolf <adolfnyerere@gmail.com>
2024-02-16 19:27:16 +0300

    committed stashed changes
    ready to merge branches
    
    next merge

--- app/main.py
+++ app/main.py
@@ -1,92 +1,92 @@
-# from flask import Flask, Blueprint, jsonify, make_response
-# from datetime import datetime
-# from flask_restful import Api, Resource, reqparse
-# from flask_marshmallow import Marshmallow
-# from marshmallow_sqlalchemy import SQLAlchemyAutoSchema,auto_field
-# from models import User, Profile, Interests, Tag, Event, Billing_Info, Billing_Details, Advert_Fees, Pricing, Review, Booking, Photo, db
-# from marshmallow import Schema, fields
-# from flask_jwt_extended import jwt_required
-# from uuid import UUID
-# from uuid import uuid4
-# from datetime import datetime
+from flask import Flask, Blueprint, jsonify, make_response
+from datetime import datetime
+from flask_restful import Api, Resource, reqparse
+from flask_marshmallow import Marshmallow
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema,auto_field
+from models import User, Profile, Interests, Tag, Event, Billing_Info, Billing_Details, Advert_Fees, Pricing, Review, Booking, Photo, db
+from marshmallow import Schema, fields
+from flask_jwt_extended import jwt_required
+from uuid import UUID
+from uuid import uuid4
+from datetime import datetime
 
 
-# main_bp = Blueprint('main', __name__)
-# app = Flask(__name__)
-# api = Api(main_bp)
-# ma = Marshmallow(main_bp)
+main_bp = Blueprint('main', __name__)
+app = Flask(__name__)
+api = Api(main_bp)
+ma = Marshmallow(main_bp)
 
 
-# class PhotoSchema(SQLAlchemyAutoSchema):
-#     class Meta:
-#         model = Photo
-# photo_schema = PhotoSchema()
+class PhotoSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Photo
+photo_schema = PhotoSchema()
 
-# class ProfileSchema(SQLAlchemyAutoSchema):
-#     class Meta:
-#         model = Profile
-# profile_schema = ProfileSchema()
+class ProfileSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Profile
+profile_schema = ProfileSchema()
 
-# class InterestsSchema(SQLAlchemyAutoSchema):
-#     class Meta:
-#         model = Interests
-#         include_fk = True  
+class InterestsSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Interests
+        include_fk = True  
 
-#     id = auto_field()
-#     event_id = auto_field()
-#     user_id = auto_field()
+    id = auto_field()
+    event_id = auto_field()
+    user_id = auto_field()
 
-# interest_schema = InterestsSchema()
+interest_schema = InterestsSchema()
 
-# class TagSchema(SQLAlchemyAutoSchema):
-#     class Meta:
-#         model = Tag
-# tag_schema = TagSchema()
+class TagSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Tag
+tag_schema = TagSchema()
 
-# class EventSchema(SQLAlchemyAutoSchema):
-#     class Meta:
-#         model = Event
-# event_schema = EventSchema()
+class EventSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Event
+event_schema = EventSchema()
 
-# class Billing_InfoSchema(SQLAlchemyAutoSchema):
-#     class Meta:
-#         model = Billing_Info
-# billing_info_schema = Billing_InfoSchema()
+class Billing_InfoSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Billing_Info
+billing_info_schema = Billing_InfoSchema()
 
-# class Billing_DetailsSchema(SQLAlchemyAutoSchema):
-#     class Meta:
-#         model = Billing_Details
-# billing_details_schema = Billing_DetailsSchema()
+class Billing_DetailsSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Billing_Details
+billing_details_schema = Billing_DetailsSchema()
 
-# class ReviewSchema(SQLAlchemyAutoSchema):
-#     class Meta:
-#         model = Review
-# review_schema = ReviewSchema()
+class ReviewSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Review
+review_schema = ReviewSchema()
 
-# class PricingSchema(SQLAlchemyAutoSchema):
-#     class Meta:
-#         model = Pricing
-# pricing_schema = PricingSchema()
+class PricingSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Pricing
+pricing_schema = PricingSchema()
 
-# class Advert_FeesSchema(SQLAlchemyAutoSchema):
-#     class Meta:
-#         model = Advert_Fees
-# advert_fees_schema = Advert_FeesSchema()
+class Advert_FeesSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Advert_Fees
+advert_fees_schema = Advert_FeesSchema()
 
-# class BookingSchema(SQLAlchemyAutoSchema):
-#     class Meta:
-#         model = Booking
-# booking_schema = BookingSchema()
+class BookingSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Booking
+booking_schema = BookingSchema()
 
-# class UserSchema(SQLAlchemyAutoSchema):
-#     class Meta:
-#         model = User
-# user_schema = UserSchema()
+class UserSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
+user_schema = UserSchema()
 
-# @main_bp.route('/')
-# def home():
-#     return 'welcome to Events projects'
+@main_bp.route('/')
+def home():
+    return 'welcome to Events projects'
 
 
-# if __name__ == '__main__':
-#     app.run(port=5555, debug=True)
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
