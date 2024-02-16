ae6e52029c787515be10fd4c90a32a4de9a4c694
Gad-Ongoro <gadongoro1@gmail.com>
2024-02-16 16:04:11 +0300

    UPDATE: Implemented MVC
    NEXT: Identity and axcess management

--- app/main.py
+++ app/main.py
@@ -1,22 +1,14 @@
-from flask import Flask, Blueprint, jsonify, make_response
+from flask import Flask, jsonify, make_response
 from datetime import datetime
 from flask_restful import Api, Resource, reqparse
-from flask_marshmallow import Marshmallow
+
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from models import User, Profile, Interests, Tag, Event, Billing_Info, Billing_Details, Advert_Fees, Pricing, Review, Booking, Photo, db
 from marshmallow import Schema, fields
 from flask_jwt_extended import jwt_required
 
-main_bp = Blueprint('main', __name__)
-app = Flask(__name__)
-api = Api(main_bp)
-ma = Marshmallow(main_bp)
-
-
-class PhotoSchema(SQLAlchemyAutoSchema):
-    class Meta:
-        model = Photo
-photo_schema = PhotoSchema()
+from app import create_app
+app = create_app()
 
 class ProfileSchema(SQLAlchemyAutoSchema):
     class Meta:
@@ -53,11 +45,6 @@ class ReviewSchema(SQLAlchemyAutoSchema):
         model = Review
 review_schema = ReviewSchema()
 
-class PricingSchema(SQLAlchemyAutoSchema):
-    class Meta:
-        model = Pricing
-pricing_schema = PricingSchema()
-
 class Advert_FeesSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = Advert_Fees
@@ -73,10 +60,9 @@ class UserSchema(SQLAlchemyAutoSchema):
         model = User
 user_schema = UserSchema()
 
-@main_bp.route('/')
+@app.route('/')
 def home():
     return 'welcome to Events projects'
-
 
 
 if __name__ == '__main__':
