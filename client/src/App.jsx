96e6eaad1ad7db3305e9017fc4fd85a8bbe3fbf3
Gad-Ongoro <gadongoro1@gmail.com>
2024-02-22 18:10:57 +0300

    feat: added Context to pass props

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { useState, useEffect} from 'react';
+import { useState, useEffect, createContext } from 'react';
 import AuthPage from './components/authpage';
 import './App.css';
 import Home from './components/home';
 import Event from './components/event';
 import EventDetails from './components/eventdetails';
+export const eventsContext = createContext();
 
 function App() {
   
@@ -21,53 +22,55 @@ function App() {
 
 
   return (
-    <Router>
-      <div id="home">
-        <div></div>
-        <div id='nav'>
-          
-          <div id='logo-stf'>
-            <div id='under-ln'><span id='top-ln'>T</span> &<span id='top-ln'> N</span></div>
-            <div id='under-ln'><span>_______   __</span></div>
-              
-          </div>
-          
+    <eventsContext.Provider value={{}}>
+      <Router>
+        <div id="home">
           <div></div>
-          
-          <div className='nav-list'>
+          <div id='nav'>
             
-            <div>
-              <input id='srch-bar' placeholder='Search Event'></input>
-            </div>
-            <div>
-              <Link id='log-list' to="/home">Home</Link>
-            </div>
-            <div>
-              <Link id='log-list' to="/event">Events</Link>
-            </div>
-            <div >
-              <Link id='log-list' to="/authpage/signup">Login</Link>
-            </div>
-            <div>
-              <Link  to="/event"><button id='tck-btn' type='button'>Buy Ticket</button></Link>
+            <div id='logo-stf'>
+              <div id='under-ln'><span id='top-ln'>T</span> &<span id='top-ln'> N</span></div>
+              <div id='under-ln'><span>_______   __</span></div>
+                
             </div>
             
+            <div></div>
             
+            <div className='nav-list'>
+              
+              <div>
+                <input id='srch-bar' placeholder='Search Event'></input>
+              </div>
+              <div>
+                <Link id='log-list' to="/home">Home</Link>
+              </div>
+              <div>
+                <Link id='log-list' to="/event">Events</Link>
+              </div>
+              <div >
+                <Link id='log-list' to="/authpage/signup">Login</Link>
+              </div>
+              <div>
+                <Link  to="/event"><button id='tck-btn' type='button'>Buy Ticket</button></Link>
+              </div>
+              
+              
+            </div>
           </div>
+          <hr />
+          <div></div>
+          {showBackButton && <button id="back-button" onClick={goBack}>Back</button>}
+          <Routes>
+          
+            <Route path="/authpage/*" element={<AuthPage />} />
+            <Route path="/home/*" element={<Home />} />
+            <Route path="/event/*" element={<Event />} />
+            <Route path="/event/:eventId/*" element={<EventDetails />} />
+          </Routes>
+          
         </div>
-        <hr />
-        <div></div>
-        {showBackButton && <button id="back-button" onClick={goBack}>Back</button>}
-        <Routes>
-        
-          <Route path="/authpage/*" element={<AuthPage />} />
-          <Route path="/home/*" element={<Home />} />
-          <Route path="/event/*" element={<Event />} />
-          <Route path="/event/:eventId" element={<EventDetails />} />
-        </Routes>
-        
-      </div>
-    </Router>
+      </Router>
+    </eventsContext.Provider>
   );
 }
 
