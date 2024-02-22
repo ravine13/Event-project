73be2a9b8ccbdd96dbd3394635a27a14cce04ef3
Gad-Ongoro <gadongoro1@gmail.com>
2024-02-22 21:44:53 +0300

    feat: POST review

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -4,6 +4,8 @@ import { useParams } from 'react-router-dom';
 function Reviews() {
 	const [reviews, setReviews] = useState([]);
 	const { eventId } = useParams();
+	const [newReview, setNewReview] = useState("");
+
 	let spinners = (<div className='text-center p-4 m-4'>
 		<div className='spinner-border text-primary mx-2'></div>
 		<div className='spinner-grow text-primary mx-2'></div>
@@ -18,9 +20,27 @@ function Reviews() {
 		.then(response => response.json())
 		.then(data => {
 			setReviews(data.reviews);
-			console.log(data.reviews);
 		})
 	}, [eventId])
+
+	function handleReviewSubmit(e){
+		e.preventDefault();
+
+		fetch(`http://localhost:5555/new_review`, {
+      		method: "POST",
+      		headers: {
+        		"Content-Type": "application/json",
+      			},
+      		body: JSON.stringify({ 'comment': newReview, 'event_id' : eventId }),
+    	})
+      .then((response) => response.json())
+      .then((data) => {
+        setReviews(current => [...current, data]);
+        setNewReview("");
+		console.log(data);
+      })
+      .catch((error) => console.error("Error submitting review:", error));
+  	};
 
 	let review_cards = reviews.map((review) => {
 			return (
@@ -33,8 +53,18 @@ function Reviews() {
   	return (
 		<div>
 			{reviews[0] !== undefined ? review_cards : spinners}
-			<form action="">
-				<textarea className='bg-white text-black' name="" id="" cols="60" rows="2"></textarea>
+			<form action="" onSubmit={handleReviewSubmit}>
+				<textarea 
+				className='bg-white text-black'
+				name="" 
+				id="" 
+				cols="60" 
+				rows="2"
+				onChange={(e) => setNewReview(e.target.value)}
+				></textarea>
+				<button className='border-0' type='submit'>
+					<img src="https://cdn-icons-png.flaticon.com/128/10337/10337579.png" alt="NA" width={30}/>
+				</button>
 			</form>
 		</div>
   	);
